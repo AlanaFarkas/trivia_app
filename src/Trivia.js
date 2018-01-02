497c7b02807f091672caaaf3fe17e72e2b1317d7
Alana Farkas <alanafarkas@Alanas-MacBook-Pro.local>
2018-01-02 14:08:22 -0500

    seperated files, added trivia stylesheet

---
+++ src/Trivia.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Trivia.css';
+
+class Trivia extends Component {
+  render() {
+    return (
+    <div className="button-container">
+        <button>Hit Me</button>
+    </div>
+    );
+  }
+}
+
+export default Trivia;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Trivia from './Trivia';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(<Trivia />, document.getElementById('trivia'));
+registerServiceWorker();
+
