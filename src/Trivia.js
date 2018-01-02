790b15e5cf39a5cf338391e6527df3ca0f4c940c
Alana Farkas <alanafarkas@Alanas-MacBook-Pro.local>
2018-01-02 14:33:47 -0500

    adds constructor and starts new method

--- src/Trivia.js
+++ src/Trivia.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './Trivia.css';
 
 class Trivia extends Component {
-  render() {
-    return (
-    <div className="button-container">
-        <button>Hit Me</button>
-    </div>
-    );
-  }
-}
+
+	constructor(props, context) {
+		super (props, context);
+		
+		this.state = {
+      		allTrivia: []
+    	}
+
+    	this.newTrivia = this.newTrivia.bind(this);
+
+	}; //constructor
+
+	newTrivia(e) {
+		// let trivia = "Alana";
+	}
+		  
+	render() {
+		return (
+		<div className="button-container">
+		    <button>Hit Me</button>
+		</div>
+		);
+	}; //render
+
+
+}; //Trivia
 
 export default Trivia;
+
+'https://opentdb.com/api.php?amount=1&category=9&difficulty=medium'
