c9447cefab63ea99340241b87a56d8456bd17273
Alana Farkas <alanafarkas@Alanas-MacBook-Pro.local>
2018-02-10 21:40:28 -0500

    does not import app.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import Question from './Question';
 import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
 
