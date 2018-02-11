07abeae0fadf9ca588006649a60a1521206e7d8f
Alana Farkas <alanafarkas@Alanas-MacBook-Pro.local>
2018-02-11 15:30:18 -0500

    adds some psuedocade

--- src/Question.js
+++ src/Question.js
@@ -2,51 +2,28 @@ import React, { Component } from 'react';
 import { Button } from 'react-materialize'
 import { Row } from 'react-materialize'
 import { Col } from 'react-materialize'
-
-
 import './styles/css/question.css';
 var axios = require('axios');
 var decode = require('unescape');
 
 
-// export default () => (
-// 	<Button waves='light'>
-// 		<Icon>thumb_up</Icon>
-// 	</Button>
-// )
  
 // var file = require('file-system');
 // var fs = require('fs');
  
 //file.readFile === fs.readFile // true 
 
-// *************
-		// fetch('https://opentdb.com/api.php?amount=1&category=9&difficulty=medium')
-		//   .then(
-		//     function(response) {
-		//       if (response.status !== 200) {
-		//         console.log('Looks like there was a problem. Status Code: ' +
-		//           response.status);
-		//         return;
-		//       }
-
-		//       // Examine the text in the response
-		//       response.json().then(function(data) {
-		//         console.log(data);
-		//       });
-		//     }
-		//   )
-		//   .catch(function(err) {
-		//     console.log('Fetch Error :-S', err);
-		//   });
-// ************
 
 class Question extends Component {
 
 	constructor(props, context) {
 		super (props, context);		
 		this.state = {
-      		questions: []
+      		questions: [],
+      		correctAnswer: [],
+      		incorrectAnswer: []
+      		// answeredCorrectly: [],
+      		// answeredIncorrectly: []
     	}
     	this.getQuestions = this.getQuestions.bind(this);
 	}; //constructor
@@ -61,7 +38,7 @@ class Question extends Component {
 		    }
 		  })
 		  .then( (response) => {
-		  	var questionObj = response.data.results;
+		  	var questionObj = decode(response.data.results);
 		  	this.state.questions.push(questionObj);
 		  	console.log(this.state.questions);
 
