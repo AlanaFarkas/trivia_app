d716d64f68c4d1bcbe67d2886b92bc7ad62b49b9
Alana Farkas <alanafarkas@Alanas-MacBook-Pro.local>
2018-02-12 21:59:35 -0500

    moves css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/css/index.css';
 // import App from './App';
-import Question from './Question';
+import Question from './question';
 import registerServiceWorker from './registerServiceWorker';
 
 
