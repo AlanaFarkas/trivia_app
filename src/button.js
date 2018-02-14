fc131205e10b576c63edf329372c322c08bb3761
Alana Farkas <alanafarkas@Alanas-MacBook-Pro.local>
2018-02-13 20:25:30 -0500

    cleaner

---
+++ src/button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'react-materialize';
+// import { Question } from './question';
+// import { Row } from 'react-materialize';
+// import { Col } from 'react-materialize';
+
+export default class HitMeButton extends Component {
+	render() {
+	return(
+		// <Button type="submit" onClick={ this.getQuestions }>Hit Me</Button>
+		<Button type="submit">Hit Me</Button>
+
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Question from './Question';
+import Question from './question';
 import registerServiceWorker from './registerServiceWorker';
 
 
